2aa387b0747b00df0b58f57dab10b6bf9a67adee
artyombakulev <i@artbakulev.com>
2019-09-17 23:26:47 +0300

    Refactor, replace relative paths with absolute, because of webpack and add pages to node server

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const Logger = require('./static/js/libs/Logger');
 const app = express();
 
-
 const log = new Logger();
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html', (e) => {
+    res.sendFile(__dirname + '/src/pages/index/index.html', (e) => {
             if (e) {
                 log.logError(404, req.url);
                 return res.status(404).send('Route ' + req.url + ' is not found.');
@@ -14,8 +13,58 @@ app.get('/', function (req, res) {
     );
 });
 
-app.get('/film-page*', function (req, res) {
-    res.sendFile(__dirname + '/public' + req.url + '.html', (e) => {
+app.get('/film', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/film/film.html', (e) => {
+            if (e) {
+                log.logError(404, req.url);
+                return res.status(404).send('Route ' + req.url + ' is not found.');
+            }
+        }
+    );
+});
+
+app.get('/search', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/search/search.html', (e) => {
+            if (e) {
+                log.logError(404, req.url);
+                return res.status(404).send('Route ' + req.url + ' is not found.');
+            }
+        }
+    );
+});
+
+app.get('/error', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/error/error.html', (e) => {
+            if (e) {
+                log.logError(404, req.url);
+                return res.status(404).send('Route ' + req.url + ' is not found.');
+            }
+        }
+    );
+});
+
+app.get('/entrance', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/entrance/entrance.html', (e) => {
+            if (e) {
+                log.logError(404, req.url);
+                return res.status(404).send('Route ' + req.url + ' is not found.');
+            }
+        }
+    );
+});
+
+app.get('/admin', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/admin/admin.html', (e) => {
+            if (e) {
+                log.logError(404, req.url);
+                return res.status(404).send('Route ' + req.url + ' is not found.');
+            }
+        }
+    );
+});
+
+app.get('/profile', function (req, res) {
+    res.sendFile(__dirname + '/src/pages/profile/profile.html', (e) => {
             if (e) {
                 log.logError(404, req.url);
                 return res.status(404).send('Route ' + req.url + ' is not found.');
