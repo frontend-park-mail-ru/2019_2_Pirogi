2766b014b73e65f39f2f309f41665261011c29fc
artyombakulev <i@artbakulev.com>
2019-09-20 13:18:00 +0300

    add js support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     mode: 'development',
     module: {
         rules: [
             {
-                test: /\.s[ac]ss$/,
-                loader: 'sass-loader',
-
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
             {
                 // Для включения в css файлов шрифтов и картинок
-                test: /\.(jp.*g|png|woff|woff2|eot|ttf|svg)$/,
+                test: /\.(jp?g|png|woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
             }
         ],
     },
