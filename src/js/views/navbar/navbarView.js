874c9877309f68a6d9768b330542a7c1dfd254c9
ansushina <nastya.sushina@yandex.ru>
2019-09-23 12:56:37 +0300

    add simple navbar events

--- src/js/views/navbar/navbarView.js
+++ src/js/views/navbar/navbarView.js
@@ -9,11 +9,36 @@ export default class NavbarView extends View {
         super(globalEventBus, root);
 
         this.globalEvetBus = globalEventBus;
+
+        this.globalEvetBus.addEventListener('onNavbarSignInClicked',
+            this.onSignInClicked.bind(this));
+        this.globalEvetBus.addEventListener('onNavbarProfileClicked',
+            this.onProfileClicked.bind(this));
     }
+
+    onSignInClicked() {
+        console.log('Go to login page');
+    }
+
+    onProfileClicked() {
+        console.log('go to profile page');
+    }
+
     /**
      * @param {object} data
      */
     render(data) {
         super.render(data);
+
+        this.isAuth = true;
+        if (this.isAuth === false) {
+            this.signInButton = document.querySelector('<div class="button">');
+            this.signInButton.addEventListener('click',
+                this.globalEvetBus.dispatchEvent('onNavbarSingInClicked'));
+        } else {
+            this.profileButton = document.getElementsByClassName('profile-button')[0];
+            this.profileButton.addEventListener('click',
+                this.globalEvetBus.dispatchEvent('onNavbarProfileClicked'));
+        }
     }
 }
