f1f99d6167d8d9800310a9348ba6fff3e1c834d1
ansushina <nastya.sushina@yandex.ru>
2019-11-23 16:43:51 +0300

    fix

--- src/js/controllers/chatController.js
+++ src/js/controllers/chatController.js
@@ -1,6 +1,7 @@
 import EventBus from '../libs/eventBus';
 import ChatView from '../views/chat/chatView';
 import ChatModel from '../models/chatModel';
+import MyWebSocket from "../libs/webSocket";
 
 const chatEvents = [
 ];
@@ -22,67 +23,75 @@ export default class ChatController {
 
         this.localEventBus.addEventListener('createWS', () => this.createWS());
         this.localEventBus.addEventListener('closeWS', () => this.closeWS());
+
+        this.socket = new MyWebSocket();
     }
 
-
-    createWS() {
-        this.socket = new WebSocket('wss://cinsear.ru/ws/');
-
-        this.socket.onopen = () => {
-            console.log('WebSocket [open] Соединение установлено');
-
-            const send = document.getElementById('js-message-sendler');
-            send.addEventListener('click', () => {
-                const input = document.getElementById('js-message-input');
-                if (input.value) {
-                    const data = {
-                        body: input.value,
-                    };
-                    console.log('WebSocket Отправляем данные на сервер' + JSON.stringify(data));
-                    this.socket.send(JSON.stringify(data));
-
-                    const date = new Date();
-                    const m = document.createElement('div');
-                    m.classList.add('mes');
-                    m.classList.add('mes_user');
-                    m.innerHTML = '<div class="mes__author">Я</div><div class="mes__body">' +
-                        input.value + '</div> <div class="mes__date">' + date + '</div>';
-                    const messages = document.querySelector('.chat__messages');
-                    messages.appendChild(m);
-                    input.value = '';
-                }
-            });
-        };
-
-        this.socket.onmessage = (event) => {
-            console.log(`WebSocket[message] Данные получены с сервера: ${event.data}`);
+    sendMessage() {
+        const input = document.getElementById('js-message-input');
+        if (input.value) {
+            const data = {
+                body: input.value,
+            };
+            console.log('WebSocket Отправляем данные на сервер' + JSON.stringify(data));
+            this.socket.send(JSON.stringify(data));
 
             const date = new Date();
             const m = document.createElement('div');
             m.classList.add('mes');
-            m.classList.add('mes_admin');
-            m.innerHTML = '<div class="mes__author">Поддержка</div><div class="mes__body">' +
-                 event.data.body + '</div> <div class="mes__date">' + date + '</div>';
+            m.classList.add('mes_user');
+            m.innerHTML = '<div class="mes__author">Я</div><div class="mes__body">' +
+                input.value + '</div> <div class="mes__date">' + date + '</div>';
             const messages = document.querySelector('.chat__messages');
             messages.appendChild(m);
-        };
+            input.value = '';
+        }
+    }
 
-        this.socket.onclose = (event) => {
-            if (event.wasClean) {
-                console.log(`WebSocket [close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
-            } else {
-                // например, сервер убил процесс или сеть недоступна
-                // обычно в этом случае event.code 1006
-                console.log('WebSocket:[close] Соединение прервано');
-            }
-        };
 
-        this.socket.onerror = (error) => {
-            console.log('WebSocket: [error] ', error);
-        };
+    createWS() {
+        this.socket.open();
+        const send = document.getElementById('js-message-sendler');
+        send.addEventListener('click', () => {
+            this.socket.send(document.getElementById('js-message-input').value)
+        });
+        // const input = document.getElementById('js-message-input');
+        // input.addEventListener('keydown', (event) => {
+        //     if (event.code === 'Enter') {
+        //         this.sendMessage();
+        //     }
+        // });
+
+        // this.socket.onmessage( (event) => {
+        //     console.log(`WebSocket[message] Данные получены с сервера: ${event.data}`);
+        //
+        //     const date = new Date();
+        //     const m = document.createElement('div');
+        //     m.classList.add('mes');
+        //     m.classList.add('mes_admin');
+        //     m.innerHTML = '<div class="mes__author">Поддержка</div><div class="mes__body">' +
+        //          event.data.body + '</div> <div class="mes__date">' + date + '</div>';
+        //     const messages = document.querySelector('.chat__messages');
+        //     messages.appendChild(m);
+        // });
+
+        // this.socket.onclose( (event) => {
+        //     if (event.wasClean) {
+        //         console.log(`WebSocket [close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`);
+        //     } else {
+        //         // например, сервер убил процесс или сеть недоступна
+        //         // обычно в этом случае event.code 1006
+        //         console.log('WebSocket:[close] Соединение прервано');
+        //     }
+        // });
+
+        // this.socket.onerror = (error) => {
+        //     console.log('WebSocket: [error] ', error);
+        // });
     }
 
     closeWS() {
         this.socket.close();
+        console.log('WebSocket:[close] Соединение закрыто с помощью close');
     }
 }
---
+++ src/js/libs/webSocket.js
@@ -0,0 +1,60 @@
+export default class MyWebSocket {
+    constructor() {
+        this.socket = null;
+    }
+
+    open() {
+        if (this.socket !== null) return;
+        this.socket = new WebSocket('wss://cinsear.ru/ws/');
+        this.socket.onopen = (data) => console.log(data);
+        this.socket.onclose = () => this.close();
+        this.socket.onerror = (data) => console.log(data);
+        this.socket.onmessage = (data) => {
+            const date = new Date();
+            const m = document.createElement('div');
+            m.classList.add('mes');
+            m.classList.add('mes_admin');
+            m.innerHTML = '<div class="mes__author">Поддержка</div><div class="mes__body">' +
+                data.data.body + '</div> <div class="mes__date">' + date + '</div>';
+            const messages = document.querySelector('.chat__messages');
+            messages.appendChild(m);
+            console.log(data);
+        };
+    }
+    //
+    // onopen() {
+    //     this.socket.onopen = (data) => console.log(data);
+    // }
+    //
+    // onclose() {
+    //     this.socket.onclose = (data) => console.log(data);
+    // }
+    //
+    // onerror() {
+    //     this.socket.onerror = (data) => console.log(data);
+    // }
+    //
+    // onmessage() {
+    //     this.socket.onmessage = (data) => {
+    //         const date = new Date();
+    //         const m = document.createElement('div');
+    //         m.classList.add('mes');
+    //         m.classList.add('mes_admin');
+    //         m.innerHTML = '<div class="mes__author">Поддержка</div><div class="mes__body">' +
+    //             data.data.body + '</div> <div class="mes__date">' + date + '</div>';
+    //         const messages = document.querySelector('.chat__messages');
+    //         messages.appendChild(m);
+    //         console.log(data);
+    //     };
+    // }
+
+    send(data) {
+        this.socket.send(JSON.stringify(data));
+    }
+
+    close() {
+        this.socket.close();
+        console.log(this.socket)
+        this.socket = null;
+    }
+};
